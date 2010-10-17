cf2a9778588b84bbaa25cefc333790585406e003
guillaume belleguic <guillaume.belleguic@cixi.eu>
2010-10-17 15:21:51 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cream::Application.config.secret_token = '7ab4e420e46619fa653baa7f89a1e4d812ff558557b8c35a209b1f7ccb65393bf5f8c0842ae31b2ed8cc82be085cb526a3f27f05f87d7cee26a713b223d7329f'
